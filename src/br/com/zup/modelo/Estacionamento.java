2aac586cea35bc75b3e6b0a7dd79b112716aa472
Carol <carolina.oliveira@zup.com.br>
2019-06-07 17:59:07 -0300

    Implementar classes Vagas e Estacionamento

--- src/br/com/zup/modelo/Estacionamento.java
+++ src/br/com/zup/modelo/Estacionamento.java
@@ -1,5 +1,57 @@
 package br.com.zup.modelo;
 
 public class Estacionamento {
+	
+	// Atributos do estacionamento
+	private double horaEntrada;
+	private double horaSaida;
+	private double valorTotal;
+	
+	// Construtor
+	public Estacionamento(double horaEntrada, double horaSaida, double valorTotal) {
+		super();
+		this.horaEntrada = horaEntrada;
+		this.horaSaida = horaSaida;
+		this.valorTotal = valorTotal;
+	}
 
+	// Getters & Setters
+	public double getHoraEntrada() {
+		return horaEntrada;
+	}
+
+	public void setHoraEntrada(double horaEntrada) {
+		this.horaEntrada = horaEntrada;
+	}
+
+	public double getHoraSaida() {
+		return horaSaida;
+	}
+
+	public void setHoraSaida(double horaSaida) {
+		this.horaSaida = horaSaida;
+	}
+
+	public double getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	
+	@Override
+	public String toString() {
+		
+		String modelo = "";
+		
+		modelo += "Hora de entrada do veículo -> " + this.getHoraEntrada() + "\n";
+		modelo += "Hora de saída do veículo -> " + this.getHoraSaida() + "\n";
+		modelo += "Valor à pagar: R$ " + this.getValorTotal() + "\n";
+		
+		return modelo;
+		
+	}
+	
+	
 }
--- src/br/com/zup/modelo/Vagas.java
+++ src/br/com/zup/modelo/Vagas.java
@@ -2,7 +2,63 @@ package br.com.zup.modelo;
 
 public class Vagas {
 
+	// Atributos das vagas
+	private String tipoVaga;
+	private boolean livre;
+	private int vagasCarro;
+	private int vagasMoto;
 	
+	// Construtor
+	public Vagas(String tipoVaga, boolean livre, int vagasCarro, int vagasMoto) {
+		super();
+		this.tipoVaga = tipoVaga;
+		this.livre = livre;
+		this.vagasCarro = vagasCarro;
+		this.vagasMoto = vagasMoto;
+	}
+
+	// Getters & Setters
+	public String getTipoVaga() {
+		return tipoVaga;
+	}
+
+	public void setTipoVaga(String tipoVaga) {
+		this.tipoVaga = tipoVaga;
+	}
+
+	public boolean isLivre() {
+		return livre;
+	}
+
+	public void setLivre(boolean livre) {
+		this.livre = livre;
+	}
+
+	public int getVagasCarro() {
+		return vagasCarro;
+	}
+
+	public void setVagasCarro(int vagasCarro) {
+		this.vagasCarro = vagasCarro;
+	}
+
+	public int getVagasMoto() {
+		return vagasMoto;
+	}
+
+	public void setVagasMoto(int vagasMoto) {
+		this.vagasMoto = vagasMoto;
+	}
 	
-	
+	@Override
+	public String toString() {
+		
+		String modelo = "";
+		
+		modelo += "Tipo da vaga -> " + this.getTipoVaga() + "\n";
+		modelo += "Disponibilidade da vaga -> " + this.isLivre() + "\n";
+		
+		return modelo;
+	}
+			
 }
